ef98a7eef8c9cadf449612b7b43911cf229c0980
joneswang <jonescwang@gmail.com>
2023-09-07 20:53:25 -0400

    update board

--- src/board.py
+++ src/board.py
@@ -5,7 +5,7 @@ import sys
 pygame.init()
 
 # 設置窗口的寬度和高度
-WIDTH, HEIGHT = 300, 400
+WIDTH, HEIGHT = 450, 600
 window = pygame.display.set_mode((WIDTH, HEIGHT))
 
 # 設置顏色
@@ -16,17 +16,23 @@ WHITE = (255, 255, 255)
 
 # 設置棋盤參數
 ROWS, COLS = 4, 3
-SQUARE_SIZE = WIDTH // COLS
+SQUARE_SIZE = 100
+
+# 計算新的起點來將棋盤置中
+GRID_OFFSET_X = (WIDTH - (COLS * SQUARE_SIZE)) // 2
+GRID_OFFSET_Y = (HEIGHT - (ROWS * SQUARE_SIZE)) // 2
 
 
 def draw_grid():
     # 繪製棋盤的網格線
     for row in range(ROWS + 1):
-        pygame.draw.line(window, BLACK, (0, row * SQUARE_SIZE),
-                         (WIDTH, row * SQUARE_SIZE), 1)
+        pygame.draw.line(window, BLACK,
+                         (GRID_OFFSET_X, GRID_OFFSET_Y + row * SQUARE_SIZE),
+                         (GRID_OFFSET_X + COLS * SQUARE_SIZE, GRID_OFFSET_Y + row * SQUARE_SIZE), 1)
     for col in range(COLS + 1):
-        pygame.draw.line(window, BLACK, (col * SQUARE_SIZE, 0),
-                         (col * SQUARE_SIZE, HEIGHT), 1)
+        pygame.draw.line(window, BLACK,
+                         (GRID_OFFSET_X + col * SQUARE_SIZE, GRID_OFFSET_Y),
+                         (GRID_OFFSET_X + col * SQUARE_SIZE, GRID_OFFSET_Y + ROWS * SQUARE_SIZE), 1)
 
     # 繪製虛線邊框
     pygame.draw.line(window, BLACK, (0, 0), (WIDTH, 0), 1)
@@ -37,20 +43,18 @@ def draw_grid():
 
 def draw_labels():
     font = pygame.font.SysFont(None, 24)
-
     # 繪製列標籤
     labels_col = ['A', 'B', 'C']
     for i, label in enumerate(labels_col):
         label_surface = font.render(label, True, BLACK)
-        window.blit(label_surface, (i * SQUARE_SIZE +
-                    SQUARE_SIZE // 2 - 10, 10))
-
+        window.blit(label_surface,
+                    (GRID_OFFSET_X + i * SQUARE_SIZE + SQUARE_SIZE // 2 - 10, GRID_OFFSET_Y - 30))
     # 繪製行標籤
     labels_row = ['1', '2', '3', '4']
     for i, label in enumerate(labels_row):
         label_surface = font.render(label, True, BLACK)
         window.blit(label_surface,
-                    (10, i * SQUARE_SIZE + SQUARE_SIZE // 2 - 10))
+                    (GRID_OFFSET_X - 30, GRID_OFFSET_Y + i * SQUARE_SIZE + SQUARE_SIZE // 2 - 10))
 
 
 def main():
