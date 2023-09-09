6f543cb13140fdc09ad695296f993e768af5df88
joneswang <jonescwang@gmail.com>
2023-09-09 09:24:27 -0400

    update the Piece class

--- src/game.py
+++ src/game.py
@@ -1,4 +1,4 @@
-from piece import Elephant, Lion, Giraffe, Chick
+from piece import Piece
 
 
 class Game:
@@ -10,14 +10,14 @@ class Game:
 
     def create_initial_board_config(self):
         self.board_config = {
-            "b1": Lion('down'),
-            "c1": Elephant('down'),
-            "b2": Chick('down'),
-            "a1": Giraffe('down'),
-            "a4": Elephant('up'),
-            "b3": Chick('up'),
-            "b4": Lion('up'),
-            "c4": Giraffe('up'),
+            "b1": Piece("L", -1),
+            "c1": Piece("E", -1),
+            "b2": Piece("C", -1),
+            "a1": Piece("G", -1),
+            "a4": Piece("E", 1),
+            "b3": Piece("C", 1),
+            "b4": Piece("L", 1),
+            "c4": Piece("G", 1),
         }
         self.storage_area_player1 = []
         self.storage_area_player2 = []
@@ -25,7 +25,7 @@ class Game:
     def initialize_setup_mode(self):
         # 這將清空棋盤，進入擺盤模式
         self.board_config = {}
-        self.storage_area_player1 = [Lion('up'), Giraffe(
-            'up'), Elephant('up'), Chick('up')]
-        self.storage_area_player2 = [Lion('down'), Giraffe(
-            'down'), Elephant('down'), Chick('down')]
+        self.storage_area_player1 = [Piece("L", 1), Piece(
+            "G", 1), Piece("E", 1), Piece("C", 1)]
+        self.storage_area_player2 = [
+            Piece("L", -1), Piece("G", -1), Piece("E", -1), Piece("C", -1)]
--- src/piece.py
+++ src/piece.py
@@ -1,99 +1,49 @@
 
 import pygame
-from const import SQUARE_SIZE, GRID_OFFSET_X, GRID_OFFSET_Y
+
+piece_type_map = {
+    "L": ("Lion", "lion"),
+    "G": ("Giraffe", "giraffe"),
+    "E": ("Elephant", "elephant"),
+    "C": ("Chick", "chick"),
+    "H": ("Chicken", "chicken")
+}
 
 
 class Piece:
-    image_file = ""  # This will be overridden in each subclass
+    def __init__(self, piece_type, player):
+        self.piece_type = piece_type
+        self.player = player
 
-    def __init__(self, direction):
-        self.direction = direction  # Direction can be 'up' or 'down'
+        # Set the name and image file name based on the piece type
+        self.name, self.image_file_name = piece_type_map[piece_type]
+
+        # Set the direction based on the player
+        self.direction = "up" if player == 1 else "down"
+
+        # Load the image based on the image file name and direction
         self.image = pygame.image.load(
-            f"assets/{self.image_file}_{direction}.png")  # Load the image here
+            f"assets/{self.image_file_name}_{self.direction}.png")
 
-    def draw(self, window, row, col):
-        # Scale the image to fit the square size
-        scaled_image = pygame.transform.scale(
-            self.image, (SQUARE_SIZE, SQUARE_SIZE))
-        x = GRID_OFFSET_X + col * SQUARE_SIZE
-        y = GRID_OFFSET_Y + row * SQUARE_SIZE
-        window.blit(scaled_image, (x, y))
+        # Set the move rules based on the piece type
+        self.move_rules = self.get_move_rules()
 
-    def get_available_moves(self, board):
-        # This method should be implemented by the subclasses to provide the available moves for a piece
-        pass
+    def get_move_rules(self):
+        # Define the move rules for each piece type
+        move_rules = {
+            "L": [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)],
+            "G": [(0, 1), (0, -1), (1, 0), (-1, 0)],
+            "E": [(1, 1), (-1, 1), (1, -1), (-1, -1)],
+            "C": [(0, 1) if self.player == 1 else (0, -1)],
+            "H": [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+        }
 
+        # Get the move rules based on the piece type
+        return move_rules[self.piece_type]
 
-class Elephant(Piece):
-    image_file = "elephant"  # Setting the image file name for the Elephant class
-
-    def __init__(self, direction):
-        super().__init__(direction)
-
-    def get_available_moves(self, board):
-        # Define the available moves for the elephant
-        if self.direction == 'up':
-            return [(1, 1), (-1, 1), (1, -1), (-1, -1)]
-        else:
+    def get_available_moves(self):
+        # Get the moves based on the piece type
+        if self.direction == "down":
             # Flip the moves for the down direction
-            return [(-x, -y) for x, y in [(1, 1), (-1, 1), (1, -1), (-1, -1)]]
-
-
-class Giraffe(Piece):
-    image_file = "giraffe"
-
-    def __init__(self, direction):
-        super().__init__(direction)
-
-    def get_available_moves(self, board):
-        # Define the available moves for the giraffe
-        if self.direction == 'up':
-            return [(0, 1), (1, 0), (0, -1), (-1, 0)]
-        else:
-            # Flip the moves for the down direction
-            return [(-x, -y) for x, y in [(0, 1), (1, 0), (0, -1), (-1, 0)]]
-
-
-class Lion(Piece):
-    image_file = "lion"
-
-    def __init__(self, direction):
-        super().__init__(direction)
-
-    def get_available_moves(self, board):
-        # Define the available moves for the lion
-        if self.direction == 'up':
-            return [(0, 1), (1, 0), (0, -1), (-1, 0), (1, 1), (-1, 1), (1, -1), (-1, -1)]
-        else:
-            # Flip the moves for the down direction
-            return [(-x, -y) for x, y in [(0, 1), (1, 0), (0, -1), (-1, 0), (1, 1), (-1, 1), (1, -1), (-1, -1)]]
-
-
-class Chick(Piece):
-    image_file = "chick"
-
-    def __init__(self, direction):
-        super().__init__(direction)
-
-    def get_available_moves(self, board):
-        # Define the available moves for the chick
-        if self.direction == 'up':
-            return [(0, -1)]
-        else:
-            # Flip the move for the down direction
-            return [(0, 1)]
-
-
-class Hen(Piece):
-    image_file = "chicken"
-
-    def __init__(self, direction):
-        super().__init__(direction)
-
-    def get_available_moves(self, board):
-        # Define the available moves for the hen
-        if self.direction == 'up':
-            return [(0, 1), (1, 0), (0, -1), (-1, 0), (-1, 1), (1, 1)]
-        else:
-            # Flip the moves for the down direction
-            return [(-x, -y) for x, y in [(0, 1), (1, 0), (0, -1), (-1, 0), (-1, 1), (1, 1)]]
+            return [(-x, -y) for x, y in self.move_rules]
+        return self.move_rules
